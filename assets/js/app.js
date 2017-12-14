396cb8bfc3e953f9f318623f23a4897368a244ec
naoangel <es.veneros@gmail.com>
2017-12-14 12:16:35 -0300

    ...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){
-  setInterval(function() {
-    $('.uno').hide();
-    $('.dos').show();
+
+setInterval(function() {
+    $('#uno').hide();
+    $('#dos').show();
   }, 2000);
+
+
+$(document).ready(function(){
+/*  
+Botones
+
+$("#sign-up").click(function(){
+   $('#dos').addClass('hidden');
+   $('#tres').removeClass('hidden');
+  });*/
+  $('#cuatro').hide();
+
+
+	$('.next').click(function {
+		$('#cuatro').show();
+/*  	var code = "";
+  	var str = ("123456789");
+  	for (var i = 0; i < 3; i++) {
+  		code += str.charAt(Math.floor(Math.random() * str.length));
+  	}
+  	alert(code);*/
+});
+
+
+  });
+
 
 
   });
